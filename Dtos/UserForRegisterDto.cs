136a8d0949e3fedcc47f4a7708ef6bd852df1759
Donald Haxhiaj <donaldhaxhiaj30@gmail.com>
2020-03-09 16:48:22 +0100

    Auth

---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4,ErrorMessage = "Password is between 4 and 8 characters")]
+        public string Password { get; set; }
+    }
+}
